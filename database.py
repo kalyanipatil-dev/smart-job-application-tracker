2c958a83ae46ade80ca753323555ce92cf8a37ce
kalyanipatil-dev <kalyanipatil119@gmail.com>
2026-08-24 00:03:27 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+def create_database():
+    conn = sqlite3.connect("jobs.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS jobs (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            company TEXT,
+            job_title TEXT,
+            country TEXT,
+            salary TEXT,
+            currency TEXT,
+            visa TEXT,
+            job_url TEXT,
+            application_date TEXT,
+            status TEXT
+        )
+    """)
+
+    conn.commit()
+    conn.close()
+
+create_database()
